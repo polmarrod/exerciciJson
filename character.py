4400ecf1fa1a6891661008b96c0dc342f5d85c52
Margaux Boileau <margaux.boileau.7e6@itb.cat>
2023-10-16 18:20:58 +0200

    character class

--- character.py
+++ character.py
@@ -1,1 +1,30 @@
-class Characther:
+class Character:
+    def __init__(
+        self,
+        edited,
+        name : str,
+        created,
+        gender,
+        skin_color,
+        hair_color,
+        height,
+        eye_color,
+        mass,
+        homeworld,
+        birth_year):
+
+        self.__edited = edited
+        self.__name = name
+        self.__created = created
+        self.__gender = gender
+        self.__skin_color = skin_color
+        self.__hair_color = hair_color
+        self.__heigth = height
+        self.__eye_color = eye_color
+        self.__mass = mass
+        self.__homeworld = homeworld
+        self.__birth_year = birth_year
+
+    @property
+    def info(self):
+        print(self.__name, self.__gender, self.__homeworld, self.__birth_year)
--- main.py
+++ main.py
@@ -1,5 +1,21 @@
 from globales import constants, getFile
+from character import Character
 
 def main ():
-    hola = getFile(";")
+
+    char = Character(
+        edited= "2014-12-20T21:17:56.891Z",
+        name= "Luke Skywalker",
+        created= "2014-12-09T13:50:51.644Z",
+        gender= "male",
+        skin_color= "fair",
+        hair_color= "blond",
+        height= "172",
+        eye_color= "blue",
+        mass= "77",
+        homeworld= 1,
+        birth_year= "19BBY")
+    char.info
+
 main()
+
