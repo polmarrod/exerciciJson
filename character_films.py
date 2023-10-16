ff11d497e6dd84b2bc655a1d396270d9509315cf
Margaux Boileau <margaux.boileau.7e6@itb.cat>
2023-10-16 18:42:11 +0200

    methods class character_films

--- character_films.py
+++ character_films.py
@@ -32,3 +32,15 @@ class Character_films(Character):
         self.__num_of_films = num_of_films
         self.__first_film = first_film
         self.__alive_at_the_end = alive_at_the_end
+    
+    @num_of_films.setter
+    def num_of_films(self, set_num_of_films):
+        self.__num_of_films = set_num_of_films
+    
+    @first_film.setter
+    def first_film(self, set_first_film):
+        self.__first_film = set_first_film
+    
+    @alive_at_the_end.setter
+    def alive_at_the_end(self, set_alive_at_the_end):
+        self.__alive_at_the_end = set_alive_at_the_end
