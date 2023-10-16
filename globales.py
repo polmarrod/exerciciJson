58feaa3ead0a25b358dcbde3e33e3be699f8750b
pol <pol.martinez.rodriguez.7e6@itb.cat>
2023-10-16 18:21:06 +0200

    method for reading json file to Characters objects

--- globales.py
+++ globales.py
@@ -1,32 +1,20 @@
-        
+import json
+from character import Characther
+
 class constants ():
-    PATHFITXEROJSON = "jugadors_basket.json"
+    PATHFITXEROJSON = "json/StarWars.json"
 
-def toClass(reader : enumerate):
-    dictionarie = {
-        constants.NAME : [],
-        constants.TEAM : [],
-        constants.POSITION : [],
-        constants.HEIGHT : [],
-        constants.WEIGHT : [],
-        constants.AGE : []
-    }
-    for index, row in reader:
-        if index != 0:
-            dictionarie[constants.NAME].append(row[0])     
-            dictionarie[constants.TEAM].append(row[1])  
-            dictionarie[constants.POSITION].append(row[2])     
-            dictionarie[constants.HEIGHT].append(row[3])  
-            dictionarie[constants.WEIGHT].append(row[4])  
-            dictionarie[constants.AGE].append(row[5])  
-    return dictionarie
+def toClass():
+    
+    return 
 
-def getFile(separador: str):
+def getFile():
     path = constants.PATHFITXEROJSON
     try:
-        fileReader = open(path, "r", encoding= "ASCII")
-        'filecsv = csv.reader(fileReader,  delimiter = separador,)'
-        return enumerate(filecsv)
+        with open('tu_archivo.json', 'r') as fileReader:
+            data = json.load(fileReader)
+        characters = [Characther(**item) for item in data]
+        return characters
     except:
         print ("Hi ha Hagut un error")
         return None
