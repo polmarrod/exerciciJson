057c2613b2b2f7e150515280aac8d1817c5c121f
Margaux Boileau <margaux.boileau.7e6@itb.cat>
2023-10-18 16:10:57 +0200

    get chars_list

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from globales import constants, getFile
 from character import Character
+import json
 
 def main ():
 
@@ -16,6 +17,18 @@ def main ():
         homeworld= 1,
         birth_year= "19BBY")
     char.info
+
+    f_json = open("./json/StarWars.json")
+    data = json.load(f_json)
+    chars_list = []
+
+    for item in data:
+        char = Character(**item["fields"])
+        char.info
+        chars_list.append(char)
+
+    #f_new= open("Characters.json", 'w')
+    #json.dump(char.__dict__, f_new, indent = 4)
 
 main()
 
