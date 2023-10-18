8a6092b56f5e5909ac69659eff53db7b61201f6d
pol <pol.martinez.rodriguez.7e6@itb.cat>
2023-10-18 16:15:41 +0200

    merge i main limpio

--- main.py
+++ main.py
@@ -6,16 +6,6 @@ def main ():
     characters = getChars()
     for char in characters:
         char.info
-
-    f_json = open("./json/StarWars.json")
-    data = json.load(f_json)
-    chars_list = []
-
-    for item in data:
-        char = Character(**item["fields"])
-        char.info
-        chars_list.append(char)
-
     #f_new= open("Characters.json", 'w')
     #json.dump(char.__dict__, f_new, indent = 4)
 
