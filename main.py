aec19482aa97ea905fdd0f7f61c681d670735da6
pol <pol.martinez.rodriguez.7e6@itb.cat>
2023-10-18 16:50:49 +0200

    linq con python

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from globales import constants, getChars
 from character import Character
+from character_films import Character_films
 import json
 
 def main ():
-    characters = getChars()
-    for char in characters:
+    character_film_list = getChars()
+    for char in character_film_list:
         char.info
+    character : Character_films = any("Luke Skywalker" in chars.__name for chars in character_film_list)
+    character.info
     #f_new= open("Characters.json", 'w')
     #json.dump(char.__dict__, f_new, indent = 4)
 
 main()
-
