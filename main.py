667ba35e566d160db390b4ba7468560a0e6050e9
pol <pol.martinez.rodriguez.7e6@itb.cat>
2023-10-23 18:33:11 +0200

    trabajo acabado

--- main.py
+++ main.py
@@ -6,13 +6,21 @@ import json
 def main ():
     character_film_list = getChars()
     for index, char in enumerate(character_film_list):
-        char.info
+        if char.getName == "Anakin Skywalker":
+            character_film_list[index].alive_at_the_end = "NO"
+            character_film_list[index].first_film = "STAR WARS I"
+            character_film_list[index].num_of_films = "6"
         if char.getName == "Luke Skywalker":
-            
-        if char.getName == "":
-
-
-    #f_new= open("Characters.json", 'w')
-    #json.dump(char.__dict__, f_new, indent = 4)
-
+            character_film_list[index].alive_at_the_end = "SI"
+            character_film_list[index].first_film = "STAR WARS IV"
+            character_film_list[index].num_of_films = "6"
+        if char.getName == "Luke Skywalker":
+            character_film_list[index].alive_at_the_end = "SI"
+            character_film_list[index].first_film = "STAR WARS IV"
+            character_film_list[index].num_of_films = "7"
+        char.info
+    newFile = open("json/Characters.json", 'w')
+    jsonString = json.dumps([char.__dict__ for char in character_film_list], indent = 4)
+    print(jsonString)
+    newFile.write(jsonString)
 main()
